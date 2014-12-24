d5f5bd1d4a9e78e421f674efac10c1ef8619888d
Ankit Sheth <ankitsheth111@gmail.com>
2014-12-24 13:00:56 +0530

    first commit

---
+++ src/com/example/v2_android_travis_ci/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.v2_android_travis_ci;
+
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+}
